1712a404eaa719ef27d61ddad6c7dd85a4eac162
GilmarDantas <skypegilmar@gmail.com>
2022-11-25 10:01:17 -0400

    Adicionar arquivos de projeto.

---
+++ Avaliação.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Consumo_de_Combustível
+{
+    partial class Consumo_de_combustivel
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label8 = new System.Windows.Forms.Label();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.TextBox5 = new System.Windows.Forms.TextBox();
+            this.TextBox4 = new System.Windows.Forms.TextBox();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label8
+            // 
+            this.Label8.AutoSize = true;
+            this.Label8.Location = new System.Drawing.Point(19, 96);
+            this.Label8.Name = "Label8";
+            this.Label8.Size = new System.Drawing.Size(15, 13);
+            this.Label8.TabIndex = 47;
+            this.Label8.Text = "id";
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(210, 251);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(86, 24);
+            this.Button2.TabIndex = 46;
+            this.Button2.Text = "Atualizar";
+            this.Button2.UseVisualStyleBackColor = true;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(210, 99);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(86, 24);
+            this.Button1.TabIndex = 45;
+            this.Button1.Text = "Cadastrar";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label7.Location = new System.Drawing.Point(19, 355);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(74, 18);
+            this.Label7.TabIndex = 44;
+            this.Label7.Text = "Situação";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(19, 223);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(78, 13);
+            this.Label6.TabIndex = 43;
+            this.Label6.Text = "Litros utilizados";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(19, 176);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(76, 13);
+            this.Label5.TabIndex = 42;
+            this.Label5.Text = "Litros inseridos";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(19, 57);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(47, 13);
+            this.Label4.TabIndex = 41;
+            this.Label4.Text = "Portador";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(218, 17);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(64, 13);
+            this.Label3.TabIndex = 40;
+            this.Label3.Text = "Capacidade";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(19, 17);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(84, 13);
+            this.Label2.TabIndex = 39;
+            this.Label2.Text = "Numero de série";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(19, 315);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(74, 13);
+            this.Label1.TabIndex = 38;
+            this.Label1.Text = "litros restantes";
+            // 
+            // TextBox5
+            // 
+            this.TextBox5.Location = new System.Drawing.Point(22, 239);
+            this.TextBox5.Name = "TextBox5";
+            this.TextBox5.Size = new System.Drawing.Size(94, 20);
+            this.TextBox5.TabIndex = 37;
+            this.TextBox5.Text = "0";
+            // 
+            // TextBox4
+            // 
+            this.TextBox4.Location = new System.Drawing.Point(22, 192);
+            this.TextBox4.Name = "TextBox4";
+            this.TextBox4.Size = new System.Drawing.Size(94, 20);
+            this.TextBox4.TabIndex = 36;
+            this.TextBox4.Text = "0";
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(22, 73);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(274, 20);
+            this.TextBox3.TabIndex = 35;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(221, 33);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(75, 20);
+            this.TextBox2.TabIndex = 34;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(22, 33);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(155, 20);
+            this.TextBox1.TabIndex = 33;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(333, 12);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DataGridView1.Size = new System.Drawing.Size(448, 426);
+            this.DataGridView1.TabIndex = 32;
+            // 
+            // Consumo_de_combustivel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Label8);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.TextBox5);
+            this.Controls.Add(this.TextBox4);
+            this.Controls.Add(this.TextBox3);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.DataGridView1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Consumo_de_combustivel";
+            this.Text = "avaliação";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label8;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox TextBox5;
+        internal System.Windows.Forms.TextBox TextBox4;
+        internal System.Windows.Forms.TextBox TextBox3;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+    }
+}
+
---
+++ Avaliação.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+namespace Consumo_de_Combustível
+{
+    public partial class Consumo_de_combustivel : Form
+    {
+
+        private SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+        private SqlCommand cmd = new SqlCommand();
+
+
+
+        public Consumo_de_combustivel()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            display();
+        }
+        public void display() //MOSTRAR DADOS DA TABELA NO DATAGRIDVIEW
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+            con.Open();
+
+            cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+
+            cmd.CommandText = @"select * from [Table]";
+
+            cmd.ExecuteNonQuery();
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            DataGridView1.DataSource = dt;
+        }
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e) // PEGAR ID DO ROW SELECIONADO NO DATAGRIDVIEW
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+            con.Open();
+            Int32 i;
+            i = Convert.ToInt32(DataGridView1.SelectedCells[0].Value.ToString());
+
+            cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from table where id = '" + i + "'";
+            cmd.ExecuteNonQuery();
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (dr.Read())
+            {
+                Label8.Text = Convert.ToString(dr.GetInt32(0));
+                TextBox1.Text = Convert.ToString(dr.GetInt32(1));
+                TextBox2.Text = Convert.ToString(dr.GetInt32(2));
+                TextBox3.Text = Convert.ToString(dr.GetString(3));
+                Label1.Text = Convert.ToString(dr.GetInt32(4));
+            }
+
+
+
+
+            // SE TEM COMBUSTIVEL, RODAR, SE NÃO, NÃO TEM COMBUSTIVEL
+
+            if (int.Parse(Label1.Text) > 0)
+            {
+                Label7.Text = "rodar";
+                Label7.ForeColor = Color.Green;
+            }
+            else
+            {
+                Label7.Text = "Sem combustivel";
+                Label7.ForeColor = Color.Red;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e) // INSERIR CARRO NA TABELA
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+            con.Open();
+
+            int numero_de_serie = Convert.ToInt32(TextBox1.Text);
+            int capacidade = Convert.ToInt32(TextBox2.Text);
+            int litros = Convert.ToInt32("0");
+            string portador = TextBox3.Text;
+
+
+            cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO [table](numero_de_serie, capacidade, portador, litros) " +
+                "Values('" + numero_de_serie + "', '" + capacidade + "', '" + portador + "', '" + litros + "')";
+            cmd.ExecuteNonQuery();
+                        
+                        
+            display();
+
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+        }
+
+        private void Button2_Click(object sender, EventArgs e) //INSERIR CONSUMO DE COMBUSTIVEL + CALCULAR CONSUMO
+        {
+            Int32 litros;
+
+            litros = int.Parse(Label1.Text);
+
+            Int32 calculo;
+            calculo = litros + int.Parse(TextBox4.Text) - int.Parse(TextBox5.Text);
+
+            if (con.State == ConnectionState.Open)
+                con.Close();
+            con.Open();
+
+            cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update [table] set litros = '" + calculo + "' WHERE id = '" + Label8.Text + "'";
+            cmd.ExecuteNonQuery();
+
+            TextBox4.Text = "0";
+            TextBox5.Text = "0";
+            display();
+        }
+
+       
+    }
+}
